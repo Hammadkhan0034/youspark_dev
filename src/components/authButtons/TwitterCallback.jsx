0906971ccce20a4453dfbc71471a4b8d0e45f9c1
Hammadkhan0034 <117932781+Hammadkhan0034@users.noreply.github.com>
2025-03-26 00:39:30 +0500

    update the code

--- src/components/authButtons/TwitterCallback.jsx
+++ src/components/authButtons/TwitterCallback.jsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../redux/userSlice";
 import API from "../../api/api";
-import { AUTH_CALLBACKS } from "../../config/urls/urls";
-import axios from "axios";
 
 export default function TwitterCallback() {
   const navigate = useNavigate();
@@ -14,47 +12,21 @@ export default function TwitterCallback() {
     const handleCallback = async () => {
       try {
         const params = new URLSearchParams(window.location.search);
-        const code = params.get("code");
-        const receivedState = params.get("state");
-        const storedState = localStorage.getItem("twitter_state");
+        const accessToken = params.get("access_token");
 
-        // Validate state to prevent CSRF attacks
-        if (!storedState || receivedState !== storedState) {
-          throw new Error("State mismatch - possible CSRF attack");
+        if (!accessToken) {
+          throw new Error("No access token found");
         }
 
-        // Get code verifier
-        const codeVerifier = localStorage.getItem("twitter_code_verifier");
-        if (!codeVerifier) {
-          throw new Error("Code verifier not found");
-        }
-
-        // Exchange the code for a Twitter access token
-        const tokenResponse = await axios.post(
-          "https://api.twitter.com/2/oauth2/token",
-          new URLSearchParams({
-            grant_type: "authorization_code",
-            client_id: import.meta.env.VITE_TWITTER_CLIENT_ID,
-            redirect_uri: AUTH_CALLBACKS.twitter,
-            code: code,
-            code_verifier: codeVerifier,
-          }),
-          {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          }
-        );
-
-        const twitterAccessToken = tokenResponse.data.access_token;
-
-        // Send the Twitter access token to your backend
+        // Send only access_token and channel to backend
         const response = await API.post("/social-sign-in", {
-          access_token: twitterAccessToken,
-          channel: "twitter",
+          access_token: accessToken,
+          channel: "twitter"
         });
 
         const { data } = response.data;
 
-        // Store access & refresh tokens
+        // Store tokens
         localStorage.setItem("access_token", data.access_token);
         if (data.refresh_token) {
           localStorage.setItem("refresh_token", data.refresh_token);
@@ -68,15 +40,11 @@ export default function TwitterCallback() {
           userStatus: data.user_status,
           userImage: data.user_image,
           firstLogin: data.first_login,
-          appName: data.app_name,
+          appName: data.app_name
         };
 
-        // Update Redux store with user data
+        // Update Redux store
         dispatch(setUser(userData));
-
-        // Clean up stored data
-        localStorage.removeItem("twitter_code_verifier");
-        localStorage.removeItem("twitter_state");
 
         // Navigate based on first login
         navigate(data.first_login ? "/user-profile" : "/home");
