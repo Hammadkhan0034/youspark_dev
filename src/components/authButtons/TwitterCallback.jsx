92b99b501fac8293c973901637ba7dd898095a23
Hammadkhan0034 <117932781+Hammadkhan0034@users.noreply.github.com>
2025-03-26 00:58:56 +0500

    update the code

--- src/components/authButtons/TwitterCallback.jsx
+++ src/components/authButtons/TwitterCallback.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../redux/userSlice";
 import API from "../../api/api";
-import axios from "axios";
 import { AUTH_CALLBACKS } from "../../config/urls/urls";
 
 export default function TwitterCallback() {
@@ -14,39 +13,16 @@ export default function TwitterCallback() {
     const handleCallback = async () => {
       try {
         const params = new URLSearchParams(window.location.search);
-        const code = params.get("code");
-        const receivedState = params.get("state");
-        const storedState = localStorage.getItem("twitter_state");
-        const codeVerifier = localStorage.getItem("twitter_code_verifier");
+        const accessToken = params.get("access_token");
 
-        // Verify state
-        if (!storedState || receivedState !== storedState) {
-          throw new Error("State mismatch - possible CSRF attack");
+        if (!accessToken) {
+          throw new Error("Missing access token");
         }
 
-        // Exchange code for access token
-        const tokenResponse = await axios.post(
-          "https://api.twitter.com/2/oauth2/token",
-          new URLSearchParams({
-            code: code,
-            grant_type: "authorization_code",
-            client_id: import.meta.env.VITE_TWITTER_CLIENT_ID,
-            redirect_uri: AUTH_CALLBACKS.twitter,
-            code_verifier: codeVerifier,
-          }),
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-            },
-          }
-        );
-
-        const { access_token } = tokenResponse.data;
-
-        // Send access token to your backend
+        // Send the access_token and channel to your backend
         const response = await API.post("/social-sign-in", {
-          access_token: access_token,
-          channel: "twitter"
+          access_token: accessToken,
+          channel: "twitter",
         });
 
         const { data } = response.data;
@@ -57,10 +33,6 @@ export default function TwitterCallback() {
           localStorage.setItem("refresh_token", data.refresh_token);
         }
 
-        // Clean up OAuth state
-        localStorage.removeItem("twitter_code_verifier");
-        localStorage.removeItem("twitter_state");
-
         // Create user object from response
         const userData = {
           id: data.id,
@@ -69,7 +41,7 @@ export default function TwitterCallback() {
           userStatus: data.user_status,
           userImage: data.user_image,
           firstLogin: data.first_login,
-          appName: data.app_name
+          appName: data.app_name,
         };
 
         // Update Redux store
@@ -77,11 +49,8 @@ export default function TwitterCallback() {
 
         // Navigate based on first login
         navigate(data.first_login ? "/user-profile" : "/home");
-
       } catch (error) {
         console.error("Twitter authentication error:", error);
-        localStorage.removeItem("twitter_code_verifier");
-        localStorage.removeItem("twitter_state");
         navigate("/signin-socials");
       }
     };
